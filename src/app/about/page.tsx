62b0255c7fbfe9c0677a6a8293030b1dc58c3d34
quocbinhgithub@gmail.com <quocbinhgithub@gmail.com>
2024-09-19 23:59:40 +0700

    feat: sub pages About us

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutUsSection from '@/app/sections/AboutPage/AboutUsSection'
+import React from 'react'
+
+const About = () => {
+  return (
+    <main>
+        <AboutUsSection />
+    </main>
+  )
+}
+
+export default About
